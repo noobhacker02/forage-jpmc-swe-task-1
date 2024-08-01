d67343519073a4eb05821a1ab4e4b3c739f1d7a8
noobhacker02 <84318735+noobhacker02@users.noreply.github.com>
2024-08-01 22:08:26 +0530

    Implemented data point calculation and ratio calculation; added unit tests

--- client_test.py
+++ client_test.py
@@ -1,3 +1,5 @@
+
+'''
 import unittest
 from client3 import getDataPoint
 
@@ -20,6 +22,47 @@ class ClientTest(unittest.TestCase):
   """ ------------ Add more unit tests ------------ """
 
 
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+'''
+
+import unittest
+from client3 import getDataPoint, getRatio
+
+class ClientTest(unittest.TestCase):
+
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected = [
+            ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2),
+            ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)
+        ]
+        for i, quote in enumerate(quotes):
+            self.assertEqual(getDataPoint(quote), expected[i])
+
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 118.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected = [
+            ('ABC', 120.48, 119.2, (120.48 + 119.2) / 2),
+            ('DEF', 120.87, 118.68, (120.87 + 118.68) / 2)
+        ]
+        for i, quote in enumerate(quotes):
+            self.assertEqual(getDataPoint(quote), expected[i])
+
+    def test_getRatio(self):
+        self.assertEqual(getRatio(120.0, 60.0), 2.0)
+        self.assertEqual(getRatio(60.0, 120.0), 0.5)
+        self.assertEqual(getRatio(0, 120.0), 0)
+        self.assertEqual(getRatio(120.0, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
